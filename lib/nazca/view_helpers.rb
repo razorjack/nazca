a43487e0e80797cbbe4390dcba4279f885935405
Jack Galanciak <jacek.galanciak@gmail.com>
2011-05-10 12:21:46 +0200

    Whitespace fixes

--- lib/nazca/view_helpers.rb
+++ lib/nazca/view_helpers.rb
@@ -30,38 +30,38 @@ module Nazca
       tag!(:nofollow => true)
     end
 
-		def canonical(href)
-			tag!(:canonical => true)
-		end
+    def canonical(href)
+      tag!(:canonical => true)
+    end
   
 
     def meta_tags(site_name, options = {})
       result = ""
-			@tags ||= {}
+      @tags ||= {}
 
-			options[:separator] ||= "|"
-			options[:reversed] ||= false
-			
-			options.merge!(@tags)
+      options[:separator] ||= "|"
+      options[:reversed] ||= false
       
-    	if options[:title].to_s.length > 0 # maybe use ActiveSupport and .present? method
-    	  title = [site_name, options[:separator], options[:title]]
-  	  else
-  	    title = [site_name]
-	    end
-	    
-			title.reverse! if options[:reverse]
-			result << content_tag(:title, title.join(' ').html_safe)
+      options.merge!(@tags)
+      
+      if options[:title].to_s.length > 0 # maybe use ActiveSupport and .present? method
+        title = [site_name, options[:separator], options[:title]]
+      else
+        title = [site_name]
+      end
+      
+      title.reverse! if options[:reverse]
+      result << content_tag(:title, title.join(' ').html_safe)
 
-     	result << tag(:meta, :name => :description, :content => options[:description]) if options[:description]
-			result << tag(:meta, :name => :keywords, :content => options[:keywords]) if options[:keywords]
+      result << tag(:meta, :name => :description, :content => options[:description]) if options[:description]
+      result << tag(:meta, :name => :keywords, :content => options[:keywords]) if options[:keywords]
       return result.html_safe
     end
   
     private
-  		# Normalizes output string so that it's tags are stripped
-			def normalize(str)
-				str.gsub(/<\/?[^>]*>/, "")
-			end
+      # Normalizes output string so that it's tags are stripped
+      def normalize(str)
+        str.gsub(/<\/?[^>]*>/, "")
+      end
   end
 end
